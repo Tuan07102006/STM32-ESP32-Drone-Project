7baf10023e92baa9226e03bf6ae785defebc616a
Tuan07102006 <tuanlkpn@gmail.com>
2026-04-13 15:34:23 +0700

    Dọn dẹp các file biên dịch nặng của PlatformIO

--- Phan_mem_EST32/.pio/build/esp32-s3-devkitc-1/libc40/Adafruit NeoPixel/Adafruit_Neopixel_RP2.cpp.d
+++
@@ -1,2 +0,0 @@
-.pio/build/esp32-s3-devkitc-1/libc40/Adafruit\ NeoPixel/Adafruit_Neopixel_RP2.cpp.o: \
- .pio/libdeps/esp32-s3-devkitc-1/Adafruit\ NeoPixel/Adafruit_Neopixel_RP2.cpp
--- Phan_mem_EST32/.pio/build/esp32-s3-devkitc-1/libf92/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32-s3-devkitc-1/libf92/WebServer/detail/mimetable.cpp.o: \
- C:/Users/tuanl/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- C:/Users/tuanl/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- C:/Users/tuanl/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
